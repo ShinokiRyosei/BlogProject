4485a6bf1a2f7e7c8787c654f7d6f2ef717c5a62
vagrant <shinoki.r.0301@gmail.com>
2016-02-02 19:55:14 +0900

    html表示完了

--- dblog/views.py
+++ dblog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from dblog.models import Article
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
+class HomeView(TemplateView):
+  template_name = 'index.html'
 def index(request):
- latest_blog_list = Article.objects.all().order_by('-post_date')[:3]
- return render_to_response('templates/index.html', {'latest_blog_list' : lastest_blog_list})
+        #latest_blog_list = Article.objects.all().order_by('-post_date')[:3]
+        t = loader.get_template('blogproject/templates/index.html')
+        c = Context({})
+        return HttpResponse(t.render(c))
 # Create your views here.
