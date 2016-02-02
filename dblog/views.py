6d0bb5157f168d0e14cfb6f0624afa5656be4891
vagrant <shinoki.r.0301@gmail.com>
2016-02-03 05:21:20 +0900

    form表示できてない

--- dblog/views.py
+++ dblog/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from dblog.models import Article
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-#from dblog.forms import ArticleForm
+from forms import ArticleForm
 
  
 class HomeView(TemplateView):
   template_name = 'index.html'
 def index(request):
+ if request.method == 'GET':
         #latest_blog_list = Article.objects.all().order_by('-post_date')[:3]
-       # article = Article()
-      #  t = loader.get_template('blogproject/templates/index.html')
-     #   c = Context({})
+        article = Article()
+        form = ArticleForm()
+        t = loader.get_template('blogproject/dblog/templates/index.html')
+        c = Context({'form': form})
         return HttpResponse(t.render(c))
       if request.method == 'POST':
          form = ArticleForm(request.POST)
